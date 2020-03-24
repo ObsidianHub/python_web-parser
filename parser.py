50fa4155eb18b18f718957601966a331bb861e5c
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-24 21:37:59 +0200

    declare main vars to parse containers from html

--- parser.py
+++ parser.py
@@ -2,4 +2,9 @@ from bs4 import BeautifulSoup
 import urllib.request
 
 req = urllib.request.urlopen('https://www.ua-football.com/sport')
-print(req)
+html = req.read()
+
+soup = BeautifulSoup(html, 'html.parser')
+news = soup.find_all('li', class_='liga-news-item')
+
+results = []
